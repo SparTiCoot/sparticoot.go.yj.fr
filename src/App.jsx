100d43cf258d676d30136c661a1864c7ac49e4ae
Vincent <vincent.vilfeu@gmail.com>
2024-10-01 10:58:19 +0200

    - Added floating animations to cubes and character using Anime.js. - Implemented main structure for Section 1 with SVG cubes and character. - Styled Section 1 and icon items with responsive layout and positioning. - Applied animations to cube groups in Section 1 using Anime.js. - Optimized SVG positioning and styles in the footer section. - Added media queries to improve responsive design in Section 1. - Adjusted positioning of cube groups for better visual layout.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import React from "react";
+import Section1 from "./components/Section1";
+/*
+import Section2 from './components/Section2';
+import Section3 from './components/Section3';
+import Section4 from './components/Section4';
+*/
+import './main.css'; 
+
 function App() {
-  return 'Hello world !'
+  return (
+    <div>
+      <Section1 />
+    </div>
+  );
 }
 
-export default App
+export default App;
